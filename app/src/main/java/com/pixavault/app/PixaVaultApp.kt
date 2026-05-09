f6f8266afc7f8ac2582806434878994a076f2abe
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-09 22:37:36 +0600

    Remove Coil imports from PixaVaultApp

--- app/src/main/java/com/pixavault/app/PixaVaultApp.kt
+++ app/src/main/java/com/pixavault/app/PixaVaultApp.kt
@@ -1,13 +1,9 @@
 package com.pixavault.app
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
 
-class PixaVaultApp : Application(), ImageLoaderFactory {
-    override fun newImageLoader(): ImageLoader {
-        return ImageLoader.Builder(this)
-            .crossfade(true)
-            .build()
+class PixaVaultApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
     }
 }
